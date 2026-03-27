8b9fd3ffb5ec2779b0038001c6a83cc1e8d406e7
Lorens <lorenscuscan@gmail.com>
2026-03-27 17:47:18 +0000

    interface "cadastro" done.

--- index.php
+++ index.php
@@ -1,53 +1,49 @@
-<html lang="en" dir="ltr">
-  <head>
-    <!-- Required meta tags -->
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr"> <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets\js\css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <title>Cadastro</title>
   </head>
   <body>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>Cadastro</h1>
-                    <form>
-                        <div class="mb-3">
-                        <label for="Nome" class="form-label">Nome completo</label>
-                        <input type="text" class="form-control" name="nome">
-                        </div>
-                        <div class="mb-3">
-                        <label for="endereco">Endereço</label>
-                        <input type="text" class="form-control" name="endereco">
-                        </div>
-                        <div class="mb-3">
-                        <label for="endereco">Telefone</label>
-                        <input type="text" class="form-control" name="telefone">
-                        </div>
-                        <div class="mb-3">
-                        <label for="email">E-mail</label>
-                        <input type="text" class="form-control" name="telefone">
-                        </div>
-                        <div class="mb-3">
-                        <label for="telefone">Data de nascimento</label>
-                        <input type="date" class="form-control" name="dt_nascimento">
-                        </div>
-                        <div class="mb-3">
-                        <input type="submit" class="btn=success">
-                        </div>
-                    </form>
+    <div class="container mt-5"> <div class="row">
+            <div class="col-md-6 offset-md-3"> <h1 class="mb-4">Cadastro</h1>
+                <form>
+                    <div class="mb-3">
+                        <label for="nome" class="form-label">Nome completo</label>
+                        <input type="text" class="form-control" name="nome" id="nome">
+                    </div>
+                    
+                    <div class="mb-3">
+                        <label for="endereco" class="form-label">Endereço</label>
+                        <input type="text" class="form-control" name="endereco" id="endereco">
+                    </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
+                    <div class="mb-3">
+                        <label for="telefone" class="form-label">Telefone</label>
+                        <input type="text" class="form-control" name="telefone" id="telefone">
+                    </div>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">E-mail</label>
+                        <input type="email" class="form-control" name="email" id="email">
+                    </div>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
-    -->
+                    <div class="mb-3">
+                        <label for="dt_nascimento" class="form-label">Data de nascimento</label>
+                        <input type="date" class="form-control" name="dt_nascimento" id="dt_nascimento">
+                    </div>
+
+                    <div class="mb-3">
+                        <input type="submit" class="btn btn-success w-100" value="Cadastrar">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   </body>
-</html
+</html>
