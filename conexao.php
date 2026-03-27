77d2f30850045c5b0d1f3187f124e26ed81320fa
Lorens <lorenscuscan@gmail.com>
2026-03-27 20:19:09 +0000

    making db of the project, and connecting

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $pass = "";
+    $bd = "company";
+
+    if (mysqli_connect($server, $user, $pass, $bd)) {
+        echo "Conectado";
+    }
+    else {
+        echo "Erro";
+    }
+?>
